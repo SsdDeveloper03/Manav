23f5217a8c542592c0a90eca38590b2c3b0da6cc
Society Issue Tracking <issuetracking25@gmail.com>
2025-12-11 11:10:05 +0530

    Added TailoringERP folder

---
+++ TailoringERP/TailoringERP/FrmMDIMain.Designer.vb
@@ -0,0 +1,105 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmMDIMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.SalesMasterToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CustomerMasterToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.XtraTabbedMdiManager1 = New DevExpress.XtraTabbedMdi.XtraTabbedMdiManager(Me.components)
+        Me.TaskMasterToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.XtraTabbedMdiManager1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SalesMasterToolStripMenuItem, Me.CustomerMasterToolStripMenuItem, Me.TaskMasterToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'SalesMasterToolStripMenuItem
+        '
+        Me.SalesMasterToolStripMenuItem.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.SalesMasterToolStripMenuItem.Name = "SalesMasterToolStripMenuItem"
+        Me.SalesMasterToolStripMenuItem.Size = New System.Drawing.Size(84, 20)
+        Me.SalesMasterToolStripMenuItem.Text = "Sales Master"
+        '
+        'CustomerMasterToolStripMenuItem
+        '
+        Me.CustomerMasterToolStripMenuItem.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.CustomerMasterToolStripMenuItem.Name = "CustomerMasterToolStripMenuItem"
+        Me.CustomerMasterToolStripMenuItem.Size = New System.Drawing.Size(110, 20)
+        Me.CustomerMasterToolStripMenuItem.Text = "Customer Master"
+        '
+        'Panel1
+        '
+        Me.Panel1.Location = New System.Drawing.Point(0, 27)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(797, 422)
+        Me.Panel1.TabIndex = 1
+        '
+        'XtraTabbedMdiManager1
+        '
+        Me.XtraTabbedMdiManager1.ClosePageButtonShowMode = DevExpress.XtraTab.ClosePageButtonShowMode.InAllTabPageHeaders
+        Me.XtraTabbedMdiManager1.MdiParent = Me
+        '
+        'TaskMasterToolStripMenuItem
+        '
+        Me.TaskMasterToolStripMenuItem.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.TaskMasterToolStripMenuItem.Name = "TaskMasterToolStripMenuItem"
+        Me.TaskMasterToolStripMenuItem.Size = New System.Drawing.Size(81, 20)
+        Me.TaskMasterToolStripMenuItem.Text = "Task Master"
+        '
+        'FrmMDIMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.IsMdiContainer = True
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "FrmMDIMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "FrmMDIMain"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.XtraTabbedMdiManager1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents SalesMasterToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CustomerMasterToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents XtraTabbedMdiManager1 As DevExpress.XtraTabbedMdi.XtraTabbedMdiManager
+    Friend WithEvents TaskMasterToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ TailoringERP/TailoringERP/Master/FrmTaskTypeMaster.Designer.vb
@@ -0,0 +1,313 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class FrmTaskTypeMaster
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.gcData = New DevExpress.XtraGrid.GridControl()
+        Me.gvData = New DevExpress.XtraGrid.Views.Grid.GridView()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.btnEdit = New System.Windows.Forms.Button()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.gbTask = New System.Windows.Forms.GroupBox()
+        Me.ColorPickEdit1 = New DevExpress.XtraEditors.ColorPickEdit()
+        Me.txtTaskType = New System.Windows.Forms.TextBox()
+        Me.txtTaskDetail = New System.Windows.Forms.TextBox()
+        Me.lblTaskDetail = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblTaskType = New System.Windows.Forms.Label()
+        Me.btnRefresh = New System.Windows.Forms.Button()
+        Me.txt_TaskType = New System.Windows.Forms.TextBox()
+        Me.lblF_LedgerName = New System.Windows.Forms.Label()
+        CType(Me.gcData, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.gvData, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.gbTask.SuspendLayout()
+        CType(Me.ColorPickEdit1.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'gcData
+        '
+        Me.gcData.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.gcData.Location = New System.Drawing.Point(8, 185)
+        Me.gcData.LookAndFeel.SkinName = "Office 2010 Blue"
+        Me.gcData.LookAndFeel.UseDefaultLookAndFeel = False
+        Me.gcData.MainView = Me.gvData
+        Me.gcData.Name = "gcData"
+        Me.gcData.Size = New System.Drawing.Size(901, 311)
+        Me.gcData.TabIndex = 268
+        Me.gcData.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.gvData})
+        '
+        'gvData
+        '
+        Me.gvData.Appearance.FooterPanel.Font = New System.Drawing.Font("Calibri", 11.25!)
+        Me.gvData.Appearance.FooterPanel.Options.UseFont = True
+        Me.gvData.Appearance.HeaderPanel.Font = New System.Drawing.Font("Calibri", 11.25!)
+        Me.gvData.Appearance.HeaderPanel.Options.UseFont = True
+        Me.gvData.Appearance.Row.Font = New System.Drawing.Font("Calibri", 11.25!)
+        Me.gvData.Appearance.Row.Options.UseFont = True
+        Me.gvData.GridControl = Me.gcData
+        Me.gvData.Name = "gvData"
+        Me.gvData.OptionsBehavior.Editable = False
+        Me.gvData.OptionsBehavior.ReadOnly = True
+        Me.gvData.OptionsLayout.Columns.StoreAllOptions = True
+        Me.gvData.OptionsView.ShowAutoFilterRow = True
+        Me.gvData.OptionsView.ShowFooter = True
+        Me.gvData.OptionsView.ShowGroupPanel = False
+        '
+        'btnExit
+        '
+        Me.btnExit.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.btnExit.BackColor = System.Drawing.SystemColors.Control
+        Me.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(360, 508)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(66, 29)
+        Me.btnExit.TabIndex = 274
+        Me.btnExit.Text = "Cl&ose"
+        Me.btnExit.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
+        Me.btnExit.UseVisualStyleBackColor = False
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.btnCancel.BackColor = System.Drawing.SystemColors.Control
+        Me.btnCancel.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCancel.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.btnCancel.Location = New System.Drawing.Point(284, 508)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(74, 29)
+        Me.btnCancel.TabIndex = 273
+        Me.btnCancel.Text = "&Cancel"
+        Me.btnCancel.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
+        Me.btnCancel.UseVisualStyleBackColor = False
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.btnDelete.BackColor = System.Drawing.SystemColors.Control
+        Me.btnDelete.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDelete.ForeColor = System.Drawing.Color.Red
+        Me.btnDelete.Location = New System.Drawing.Point(210, 508)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(72, 29)
+        Me.btnDelete.TabIndex = 272
+        Me.btnDelete.Text = "&Delete"
+        Me.btnDelete.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
+        Me.btnDelete.UseVisualStyleBackColor = False
+        '
+        'btnSave
+        '
+        Me.btnSave.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.btnSave.BackColor = System.Drawing.SystemColors.Control
+        Me.btnSave.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSave.ForeColor = System.Drawing.Color.DarkGreen
+        Me.btnSave.Location = New System.Drawing.Point(144, 508)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(64, 29)
+        Me.btnSave.TabIndex = 271
+        Me.btnSave.Text = "&Save"
+        Me.btnSave.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
+        Me.btnSave.UseVisualStyleBackColor = False
+        '
+        'btnEdit
+        '
+        Me.btnEdit.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.btnEdit.BackColor = System.Drawing.SystemColors.Control
+        Me.btnEdit.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnEdit.ForeColor = System.Drawing.Color.Maroon
+        Me.btnEdit.Location = New System.Drawing.Point(70, 508)
+        Me.btnEdit.Name = "btnEdit"
+        Me.btnEdit.Size = New System.Drawing.Size(72, 29)
+        Me.btnEdit.TabIndex = 270
+        Me.btnEdit.Text = "&Modify"
+        Me.btnEdit.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
+        Me.btnEdit.UseVisualStyleBackColor = False
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.btnAdd.BackColor = System.Drawing.SystemColors.Control
+        Me.btnAdd.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAdd.ForeColor = System.Drawing.Color.Blue
+        Me.btnAdd.Location = New System.Drawing.Point(8, 508)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(60, 29)
+        Me.btnAdd.TabIndex = 269
+        Me.btnAdd.Text = "&New"
+        Me.btnAdd.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
+        Me.btnAdd.UseVisualStyleBackColor = False
+        '
+        'gbTask
+        '
+        Me.gbTask.Controls.Add(Me.ColorPickEdit1)
+        Me.gbTask.Controls.Add(Me.txtTaskType)
+        Me.gbTask.Controls.Add(Me.txtTaskDetail)
+        Me.gbTask.Controls.Add(Me.lblTaskDetail)
+        Me.gbTask.Controls.Add(Me.Label2)
+        Me.gbTask.Controls.Add(Me.lblTaskType)
+        Me.gbTask.Font = New System.Drawing.Font("Calibri", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.gbTask.Location = New System.Drawing.Point(8, 5)
+        Me.gbTask.Name = "gbTask"
+        Me.gbTask.Size = New System.Drawing.Size(901, 174)
+        Me.gbTask.TabIndex = 275
+        Me.gbTask.TabStop = False
+        Me.gbTask.Text = "Task Entry"
+        '
+        'ColorPickEdit1
+        '
+        Me.ColorPickEdit1.EditValue = System.Drawing.Color.Empty
+        Me.ColorPickEdit1.Location = New System.Drawing.Point(115, 139)
+        Me.ColorPickEdit1.Name = "ColorPickEdit1"
+        Me.ColorPickEdit1.Properties.AutomaticColor = System.Drawing.Color.Black
+        Me.ColorPickEdit1.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.ColorPickEdit1.Size = New System.Drawing.Size(159, 20)
+        Me.ColorPickEdit1.TabIndex = 289
+        '
+        'txtTaskType
+        '
+        Me.txtTaskType.Location = New System.Drawing.Point(115, 30)
+        Me.txtTaskType.Name = "txtTaskType"
+        Me.txtTaskType.Size = New System.Drawing.Size(159, 26)
+        Me.txtTaskType.TabIndex = 288
+        '
+        'txtTaskDetail
+        '
+        Me.txtTaskDetail.Location = New System.Drawing.Point(115, 68)
+        Me.txtTaskDetail.Multiline = True
+        Me.txtTaskDetail.Name = "txtTaskDetail"
+        Me.txtTaskDetail.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtTaskDetail.Size = New System.Drawing.Size(233, 55)
+        Me.txtTaskDetail.TabIndex = 287
+        '
+        'lblTaskDetail
+        '
+        Me.lblTaskDetail.AutoSize = True
+        Me.lblTaskDetail.Location = New System.Drawing.Point(18, 85)
+        Me.lblTaskDetail.Name = "lblTaskDetail"
+        Me.lblTaskDetail.Size = New System.Drawing.Size(74, 18)
+        Me.lblTaskDetail.TabIndex = 286
+        Me.lblTaskDetail.Text = "Task Detail"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(20, 140)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(82, 18)
+        Me.Label2.TabIndex = 285
+        Me.Label2.Text = "Select Color"
+        '
+        'lblTaskType
+        '
+        Me.lblTaskType.AutoSize = True
+        Me.lblTaskType.Location = New System.Drawing.Point(20, 33)
+        Me.lblTaskType.Name = "lblTaskType"
+        Me.lblTaskType.Size = New System.Drawing.Size(66, 18)
+        Me.lblTaskType.TabIndex = 282
+        Me.lblTaskType.Text = "Task Type"
+        '
+        'btnRefresh
+        '
+        Me.btnRefresh.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnRefresh.BackColor = System.Drawing.SystemColors.Control
+        Me.btnRefresh.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnRefresh.Location = New System.Drawing.Point(830, 508)
+        Me.btnRefresh.Name = "btnRefresh"
+        Me.btnRefresh.Size = New System.Drawing.Size(79, 29)
+        Me.btnRefresh.TabIndex = 276
+        Me.btnRefresh.Text = "Re&fresh"
+        Me.btnRefresh.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
+        Me.btnRefresh.UseVisualStyleBackColor = False
+        '
+        'txt_TaskType
+        '
+        Me.txt_TaskType.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.txt_TaskType.Font = New System.Drawing.Font("Calibri", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txt_TaskType.Location = New System.Drawing.Point(537, 511)
+        Me.txt_TaskType.MaxLength = 50
+        Me.txt_TaskType.Name = "txt_TaskType"
+        Me.txt_TaskType.Size = New System.Drawing.Size(136, 26)
+        Me.txt_TaskType.TabIndex = 277
+        '
+        'lblF_LedgerName
+        '
+        Me.lblF_LedgerName.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.lblF_LedgerName.AutoSize = True
+        Me.lblF_LedgerName.Font = New System.Drawing.Font("Calibri", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblF_LedgerName.ForeColor = System.Drawing.Color.Black
+        Me.lblF_LedgerName.Location = New System.Drawing.Point(456, 515)
+        Me.lblF_LedgerName.Name = "lblF_LedgerName"
+        Me.lblF_LedgerName.Size = New System.Drawing.Size(66, 18)
+        Me.lblF_LedgerName.TabIndex = 278
+        Me.lblF_LedgerName.Text = "Task Type"
+        '
+        'FrmTaskTypeMaster
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(917, 549)
+        Me.Controls.Add(Me.txt_TaskType)
+        Me.Controls.Add(Me.lblF_LedgerName)
+        Me.Controls.Add(Me.btnRefresh)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCancel)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.btnEdit)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.gcData)
+        Me.Controls.Add(Me.gbTask)
+        Me.Name = "FrmTaskTypeMaster"
+        Me.Text = "Task Master"
+        CType(Me.gcData, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.gvData, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.gbTask.ResumeLayout(False)
+        Me.gbTask.PerformLayout()
+        CType(Me.ColorPickEdit1.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents gcData As DevExpress.XtraGrid.GridControl
+    Friend WithEvents gvData As DevExpress.XtraGrid.Views.Grid.GridView
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnCancel As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents btnSave As Button
+    Friend WithEvents btnEdit As Button
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents gbTask As GroupBox
+    Friend WithEvents txtTaskDetail As TextBox
+    Friend WithEvents lblTaskDetail As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lblTaskType As Label
+    Friend WithEvents btnRefresh As Button
+    Friend WithEvents txt_TaskType As TextBox
+    Friend WithEvents lblF_LedgerName As Label
+    Friend WithEvents ColorPickEdit1 As DevExpress.XtraEditors.ColorPickEdit
+    Friend WithEvents txtTaskType As TextBox
+End Class
